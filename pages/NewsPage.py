8976d0f47035e98b190134579d8d645cb6263615
Mariia <maria24547@gmail.com>
2023-06-09 03:26:51 -0400

    add NewsPage

---
+++ pages/NewsPage.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+from pages.BasePage import BasePage
+from logs.logger import logger
+
+class NewsPage(BasePage):
+    def news_button(self):
+        self.driver.find_element(By.CSS_SELECTOR, "div:nth-child(5) > label").click()
